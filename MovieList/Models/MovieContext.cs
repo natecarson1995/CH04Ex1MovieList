f7ff93c2aeb1eea4f42d55ba950c93685666178d
carson29 <carson29@mail.nmc.edu>
2021-10-03 11:15:05 -0400

    Handson 3

--- MovieList/Models/MovieContext.cs
+++ MovieList/Models/MovieContext.cs
@@ -13,6 +13,31 @@ namespace MovieList.Models
         {
             
         }
-
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().HasData(
+                new Movie
+                {
+                    MovieId = 1,
+                    Name = "Dear Evan Hansen",
+                    Year = 2021,
+                    Rating = 2
+                },
+                new Movie
+                {
+                    MovieId = 2,
+                    Name = "Malignant",
+                    Year = 2021,
+                    Rating = 4
+                },
+                new Movie
+                {
+                    MovieId = 3,
+                    Name = "Cry Macho",
+                    Year = 2021,
+                    Rating = 3
+                }
+                );
+        }
     }
 }
