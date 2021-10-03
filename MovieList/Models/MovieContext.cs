adfd70ea9fd0efff20bf45f0945235991778165b
carson29 <carson29@mail.nmc.edu>
2021-10-03 11:07:37 -0400

    Handson 2

---
+++ MovieList/Models/MovieContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieList.Models
+{
+    public class MovieContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options)
+        {
+            
+        }
+
+    }
+}
